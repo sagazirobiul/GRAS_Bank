ce8d383a4ec4aef1306016b3eaf8f5f67e625abc
sagazirobiul <gazisagor00000@gmail.com>
2021-02-19 04:08:57 +0600

    added condition

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,6 @@
 // Login button event handler
-
 const loginBtn = document.getElementById('login');
-loginBtn.addEventListener('click', function(){
+loginBtn.addEventListener('click', function () {
     const loginArea = document.getElementById('login-area');
     loginArea.style.display = "none";
     const transactionArea = document.getElementById('transaction-area');
@@ -10,36 +9,46 @@ loginBtn.addEventListener('click', function(){
 
 
 // Deposit button event handler
-
 const depositBtn = document.getElementById('deposit-btn');
-depositBtn.addEventListener('click', function(){
-    const depositNumber = getInputNumber('deposit-amount');
-    
-    updateData("current-deposit", depositNumber);
-    updateData("currentBalance", depositNumber);
-    depositAmount = document.getElementById('deposit-amount').value = "";
+depositBtn.addEventListener('click', function () {
+    const depositInput = document.getElementById('deposit-amount').value;
+    if (depositInput > 0) {
+        const depositNumber = getInputNumber('deposit-amount');
+        console.log(depositNumber);
+        updateData("current-deposit", depositNumber);
+        updateData("currentBalance", depositNumber);
+        depositAmount = document.getElementById('deposit-amount').value = "";
+    }
 });
 
 
 // Withdraw button event handler
-
 const withdrawBtn = document.getElementById('withdraw-btn');
-withdrawBtn.addEventListener('click', function(){
-    const withdrawNumber = getInputNumber("withdrawAmount");
-    updateData("current-withdraw", withdrawNumber);
-    updateData("currentBalance", -1 * withdrawNumber);
-    depositAmount = document.getElementById('withdrawAmount').value = "";
+withdrawBtn.addEventListener('click', function () {
+    const withdrawInput = document.getElementById('withdrawAmount').value;
+    if (withdrawInput > 0) {
+        const withdrawNumber = getInputNumber("withdrawAmount");
+        updateData("current-withdraw", withdrawNumber);
+        updateData("currentBalance", -1 * withdrawNumber);
+        depositAmount = document.getElementById('withdrawAmount').value = "";
+    }
 })
 
-function getInputNumber(id){
+
+function getInputNumber(id) {
     const amount = document.getElementById(id).value;
     const number = parseFloat(amount);
     return number;
 }
 
-function updateData(id, depositNumber) {
+
+function updateData(id, numberVar) {
     const current = document.getElementById(id).innerText;
     const currentNumber = parseFloat(current);
-    const total = depositNumber + currentNumber;
+    const total = numberVar + currentNumber;
     document.getElementById(id).innerText = total;
 }
+
+
+
+// Thank you :)
